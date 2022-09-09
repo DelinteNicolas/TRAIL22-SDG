400e47c74be328ed322d7deb46e72f2bbcaae8d1
Yannick Molinghen <yannick.molinghen@ulb.be>
2022-09-09 13:07:44 +0000

    Random forest classifier updated

--- src/examples/random_forest.py
+++ src/examples/random_forest.py
@@ -4,11 +4,10 @@ import numpy as np
 
 if __name__ == "__main__":
     tk = sdg.tokenizers.lemmatize_stem
-    model, vectorizer = sdg.models.random_forest_classifier(tk)
+    clf = sdg.models.random_forest_classifier(tk)
     _, _, test_x, test_y = sdg.dataset.load_sdg()
-    test_x = vectorizer.transform([tk(x) for x in test_x])
     test_y = np.array(test_y) - 1 # Because SDG labels start at 1
-    predicted_probs = model.predict_proba(test_x)
+    predicted_probs = clf(test_x)
     predicted_labels = np.argmax(predicted_probs, axis=-1)
     sdg.utils.eval_classifier(17, test_y, predicted_labels, predicted_probs)
     
--- src/sdg/models/random_forest.py
+++ src/sdg/models/random_forest.py
@@ -1,12 +1,18 @@
-from typing import Callable
+from typing import Callable, List
 import sdg
 from sklearn.ensemble import RandomForestClassifier
 
 
-def random_forest_classifier(tokenizer: Callable[[str], str]) -> RandomForestClassifier:
+def random_forest_classifier(tokenizer: Callable[[str], str]) -> Callable[[str], List[float]]:
     train_x, train_y, _, _ = sdg.dataset.load_sdg()
     vectorizer = sdg.utils.get_vectorizer()
     train_x = vectorizer.fit_transform([tokenizer(s) for s in train_x])
     classifier = RandomForestClassifier(n_estimators=100)
     classifier.fit(train_x, train_y)
-    return classifier, vectorizer
+
+    def classify(s: str):
+        s = tokenizer(s)
+        v = vectorizer.transform(s)
+        return classifier.predict_proba(v)
+
+    return classify
