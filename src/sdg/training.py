2db31a437673c9d8c2ed8955608e83e7a874773f
Yannick Molinghen <yannick.molinghen@ulb.be>
2022-09-12 14:05:09 +0000

    Training

--- src/sdg/training.py
+++ src/sdg/training.py
@@ -1,8 +1,9 @@
 from typing import Tuple
 import torch
+import os
 import numpy as np
 from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer, AutoTokenizer, PreTrainedTokenizer
-from datasets import load_metric
+from datasets import load_metric, DatasetDict
 
 from .utils.dataset import load_sdg_dataset
 
@@ -13,12 +14,78 @@ def compute_metrics(eval_pred):
     metric = load_metric("accuracy")
     return metric.compute(predictions=predictions, references=labels)
 
+
+def to_binary(dataset: DatasetDict, label_to_extract: int) -> DatasetDict:
+    def map_function(line):
+        label = line["label"]
+        if label == label_to_extract:
+            label = 1
+        else:
+            label = 0
+        line["label"] = label
+        return line
+
+    return {
+        "train": dataset["train"].map(map_function),
+        "test": dataset["test"].map(map_function)
+    } 
+
+
+def train_17_classifiers(save_directory: str, n_epochs: int):
+    base_model = "bert-base-cased"
+    tokenizer = AutoTokenizer.from_pretrained(base_model)
+    
+    os.makedirs(save_directory, exist_ok=True)
+    labels = [
+        "SDG1: No Poverty", 
+        "SDG2: Zero Hunger", 
+        "SDG3: Good Health and Well-being", 
+        "SDG4: Quality Education", 
+        "SDG5: Gender Equality", 
+        "SDG6: Clean Water and Sanitation",
+        "SDG7: Affordable and Clean Energy",
+        "SDG8: Decent Work and Economic Growth",
+        "SDG9: Industry, Innovation and Infrastructure",
+        "SDG10: Reduced Inequality",
+        "SDG11: Sustainable Cities and Communities",
+        "SDG12: Responsible Consumption and Production",
+        "SDG13: Climate Action",
+        "SDG14: Life Below Water",
+        "SDG15: Life on Land",
+        "SDG16: Peace and Justice Strong Institutions",
+        "SDG17: Partnerships to achieve the Goal"
+    ]
+    models = []
+    for i, label in enumerate(labels):
+        dataset = load_sdg_dataset(tokenizer)
+        model_dataset = to_binary(dataset, i + 1)
+        model = AutoModelForSequenceClassification.from_pretrained(base_model, num_labels=2, id2label={0: "None", 1: label})
+        training_args = TrainingArguments(
+            output_dir="test_trainer", 
+            evaluation_strategy="epoch",
+            num_train_epochs=n_epochs
+        )
+
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=model_dataset["train"],
+            eval_dataset=model_dataset["test"],
+            compute_metrics=compute_metrics
+        )
+
+        trainer.train()
+        models.append(model)
+        model.save_pretrained(os.path.join(save_directory, f"model_sdg_{i}"))
+    
+    
+
 def fine_tune_bert(save_location: str, n_epochs=3) -> Tuple[torch.nn.Module, PreTrainedTokenizer]:
     tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
     dataset = load_sdg_dataset(tokenizer)
     id2label = {
         0: "Easter egg",
-        1: "SDG1: No Poverty",
+        1: "SDG1: No Poverty", 
         2: "SDG2: Zero Hunger",
         3: "SDG3: Good Health and Well-being",
         4: "SDG4: Quality Education",
